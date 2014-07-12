6efee01d0278333eb667b2a08b583cdd0815d142
Matt Dahl <matt.dahl.2013@gmail.com>
2014-07-12 18:50:19 -0400

    Adds current filters on results page.

--- static/js/controllers/results.js
+++ static/js/controllers/results.js
@@ -1,4 +1,5 @@
 SF.StartupFinder.Controllers.controller('ResultsController', ['$scope', function ($scope) {
+	$scope.current_filters = $scope.search_options.locations.concat($scope.search_options.sizes, $scope.search_options.languages);
 
 	// Set up auto-tag conversion
 	$('.add-filter__option').children('input').each(function (index, element) {
