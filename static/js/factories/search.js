d857670dcdd2e74b64feddd169ac03f8fc6cfbf6
Matt Dahl <matt.dahl.2013@gmail.com>
2014-07-12 13:39:41 -0400

    Rough implementation of dummy search results.

--- static/js/factories/search.js
+++ static/js/factories/search.js
@@ -6,11 +6,12 @@ SF.StartupFinder.Factories.factory('Search', ['$http', '$q', function ($http, $q
 			var deferred = $q.defer();
 
 			$http.get('companies')
-			.success(function (data, status, headers, config) {
-				deferred.resolve(data);
-			}).error(function (data, status, headers, config) {
-				deferred.reject(data);
-			});
+				.success(function (data, status, headers, config) {
+					deferred.resolve(data);
+				}).error(function (data, status, headers, config) {
+					deferred.reject(data);
+				});
+
 			return deferred.promise;
 		}
 	};
