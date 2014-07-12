7fe1e6ff14b769374ac511275e54b26900423c73
Matt Dahl <matt.dahl.2013@gmail.com>
2014-07-12 17:05:13 -0400

    Basic styling and layout of results page.

--- static/js/controllers/results.js
+++ static/js/controllers/results.js
@@ -1,3 +1,8 @@
 SF.StartupFinder.Controllers.controller('ResultsController', ['$scope', function ($scope) {
 
+	// Set up auto-tag conversion
+	$('.add-filter__option').children('input').each(function (index, element) {
+		$(element).tagsInput();
+	});
+
 }]);
